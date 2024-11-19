f07cbf383b43a0dbdb72651c9aa0cb030d8a68fd
Kevin Marwick <6107320@cegepmontpetit.ca>
2024-11-18 23:55:00 -0500

    FCT Connexion Utilisateur
    Mise en place de la fonction de connexion d'un utilisateur existant

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,38 +12,46 @@ export class LoginComponent implements OnInit {
 
   hide = true;
 
-  registerUsername : string = "";
-  registerEmail : string = "";
-  registerPassword : string = "";
-  registerPasswordConfirm : string = "";
+  registerUsername: string = "";
+  registerEmail: string = "";
+  registerPassword: string = "";
+  registerPasswordConfirm: string = "";
 
-  loginUsername : string = "";
-  loginPassword : string = "";
+  loginUsername: string = "2";
+  loginPassword: string = "2";
 
   domain: string = "https://localhost:7166/"
-  urlRegister:string = "api/users/register"
+  urlRegister: string = "api/users/register"
+  urlLogin:string = "api/users/login"
 
-  constructor(public route : Router, public http:HttpClient) { }
+  constructor(public route: Router, public http: HttpClient) { }
 
   ngOnInit() {
   }
 
-  login(){
+  async login(): Promise<void>{
+    let loginDTO = {
+      username: this.loginUsername,
+      password: this.loginPassword,
+    };
+    let x = await lastValueFrom(this.http.post<any>(this.domain+this.urlLogin, loginDTO))
+    console.log(x)
+    localStorage.setItem("token", x.token)
 
     // Redirection si la connexion a réussi :
     this.route.navigate(["/play"]);
   }
 
-  async register(): Promise<void> {
-    let registerDTO = {
-        username: this.registerUsername,
-        email: this.registerEmail,
-        password: this.registerPassword,
-        passwordConfirm: this.registerPasswordConfirm
-    };
+  async register(): Promise < void> {
+  let registerDTO = {
+    username: this.registerUsername,
+    email: this.registerEmail,
+    password: this.registerPassword,
+    passwordConfirm: this.registerPasswordConfirm
+  };
 
-    let x = await lastValueFrom(this.http.post<any>(this.domain + this.urlRegister, registerDTO));
-    console.log(x);
+  let x = await lastValueFrom(this.http.post<any>(this.domain + this.urlRegister, registerDTO));
+  console.log(x);
 }
 
 
