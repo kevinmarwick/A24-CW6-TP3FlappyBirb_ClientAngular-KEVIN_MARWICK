551f5ed963c2e9b46dac7bf0057086bcd7839225
Kevin Marwick <6107320@cegepmontpetit.ca>
2024-11-18 22:32:35 -0500

    FCT Register Utilisateur
    Mise en place de la fonction enregstrer un nouvel utilisateur

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -18,20 +20,31 @@ export class LoginComponent implements OnInit {
   loginUsername : string = "";
   loginPassword : string = "";
 
-  constructor(public route : Router) { }
+  domain: string = "https://localhost:7166/"
+  urlRegister:string = "api/users/register"
+
+  constructor(public route : Router, public http:HttpClient) { }
 
   ngOnInit() {
   }
 
   login(){
 
-
     // Redirection si la connexion a réussi :
     this.route.navigate(["/play"]);
   }
 
-  register(){
+  async register(): Promise<void> {
+    let registerDTO = {
+        username: this.registerUsername,
+        email: this.registerEmail,
+        password: this.registerPassword,
+        passwordConfirm: this.registerPasswordConfirm
+    };
 
-  }
+    let x = await lastValueFrom(this.http.post<any>(this.domain + this.urlRegister, registerDTO));
+    console.log(x);
+}
+
 
 }
